e7efb248494a844a27c80f629996408360887e74
Jonathan Leverenz <jonlev03@gmail.com>
2022-07-11 15:07:39 -0400

    started the login page

---
+++ src/globalStyles.js
@@ -0,0 +1,6 @@
+export var colors = {
+  tan: "#F9F2ED",
+  blue: "#3AB0FF",
+  orange: "#FFB562",
+  red: "#F87474",
+};
---
+++ src/pages/Landing.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+const Landing = (props) => {
+  return <div></div>;
+};
+
+export default Landing;
